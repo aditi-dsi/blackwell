20296d1bf8a4d31f65d80c7f5efc082ad91dc063
Aditi Bindal <123075271+aditi-dsi@users.noreply.github.com>
2024-06-10 15:44:47 +0530

    Update BWContext.tsx

--- frontend/src/context/BWContext.tsx
+++ frontend/src/context/BWContext.tsx
@@ -1,25 +1,23 @@
 import { Contract, ethers } from "ethers";
 import { createContext } from "react";
-import stakerAbi from "./ABI/stakerAbi.json";
-import stakeTokenAbi from "./ABI/stakeTokenAbi.json";
 
 
 // Define interface for variables circulated as context
 interface BWContextType {
-    stakerContract: Contract | null; 
-    stakeTokenContract: Contract | null;
+    stakerContract: any; 
+    stakeTokenContract: any;
     selectedAccount: string;
     chainId: number;
-    provider:ethers.BrowserProvider | null;
+    provider:any;
   }
 
 // Define default values of cotnext variables
 const BWContextDefault:BWContextType = {
-    stakerContract: null , 
-    stakeTokenContract: null ,
+    stakerContract: {} , 
+    stakeTokenContract: {} ,
     selectedAccount: "",
     chainId: 0,
-    provider:null
+    provider:{}
 }
 
 // Create context
